1afc02c0b6e1fe9d8646306577f84c7933a40778
Vladislav <bshkrvv@gmail.com>
2023-03-10 20:01:31 +0300

    Rewrite script using classes

--- main.py
+++ main.py
@@ -4,6 +4,45 @@ import argparse
 import pychromecast
 import time
 
+
+class QuietHours:
+    excluded: list[str]
+    browser: pychromecast.CastBrowser
+    casts: set[pychromecast.Chromecast] = set()
+
+    def __init__(self, args):
+        self.excluded = args.exclude if args.exclude is not None else []
+
+    def discover(self):
+        print("Discovering ...")
+        self.browser = pychromecast.get_chromecasts(blocking = False, callback = self.add_cast)
+
+    def stop_discovery(self):
+        self.browser.stop_discovery()
+        print("Stopped discovery")
+
+    def add_cast(self, cast: pychromecast.Chromecast):
+        print(f"Discovered new device \"{cast.name}\"")
+
+        if cast.name in self.excluded:
+            print(f"\"{cast.name}\" is excluded")
+        else:
+            self.casts.add(cast)
+
+    def mute(self):
+        for cast in self.casts:
+            print(f"Waiting for \"{cast.name}\" to become ready")
+            cast.wait()
+            mc = cast.media_controller
+
+            if mc.status.player_is_playing:
+                print(f"\"{cast.name}\" is playing \"{mc.status.title}\" on \"{cast.app_display_name}\"")
+
+                if not cast.status.volume_muted:
+                    cast.set_volume_muted(True)
+                    print(f"Muted \"{cast.name}\"")
+
+
 parser = argparse.ArgumentParser(
     prog = "Quiet Hours",
     description = "Mute Chromecast devices"
@@ -21,40 +60,13 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-excluded_names: list[str] = args.exclude if args.exclude is not None else []
-browser: pychromecast.CastBrowser = None
-chromecasts: set[pychromecast.Chromecast] = set()
-
-def discover():
-    print("Discovering ...")
-    global browser
-    browser = pychromecast.get_chromecasts(blocking = False, callback = add_chromecast)
-
-def add_chromecast(chromecast: pychromecast.Chromecast):
-    print(f"Discovered new chromecast \"{chromecast.name}\"")
-
-    if chromecast.name in excluded_names:
-        print(f"\"{chromecast.name}\" is excluded")
-    else:
-        chromecasts.add(chromecast)
-
-def mute():
-    for chromecast in chromecasts:
-        chromecast.wait()
-        mc = chromecast.media_controller
-
-        if mc.status.player_is_playing:
-            print(f"\"{chromecast.name}\" is playing \"{mc.status.title}\" on \"{chromecast.app_display_name}\"")
-
-            if not chromecast.status.volume_muted:
-                chromecast.set_volume_muted(True)
-                print(f"Muted \"{chromecast.name}\"")
-
 try:
-    discover()
+    qh = QuietHours(args)
+    qh.discover()
 
     while True:
-        mute()
+        qh.mute()
         time.sleep(5)
+
 except KeyboardInterrupt:
-    browser.stop_discovery()
+    qh.stop_discovery()
